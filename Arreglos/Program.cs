24cb176ed949121a001530979ce50c8cc087576e
4s3mon <74688883+4s3mon@users.noreply.github.com>
2020-11-18 18:52:25 -0700

    Clase 13/11/20

--- Arreglos/Program.cs
+++ Arreglos/Program.cs
@@ -6,7 +6,34 @@ namespace Arreglos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i;
+            // int[] arreglo1;
+            // arreglo1 = new int [10];
+
+
+            // int[] arreglo2 = new int[10];
+            // arreglo1[0] = 123;
+
+            // for( i = 0; i <= arreglo1.Length; i++){
+            //     Console.Write("Ingresa un valor para el arreglo enla posicion # " + i + ": ");
+            //     num = int.Parse(Console.ReadLine());
+            //     arreglo1[i] = num;
+            // }
+            // Console.WriteLine("Indice      Valor");
+            // for(i = 0; i <= arreglo1.Length; i++){
+            //     Console.WriteLine("  " + i + "           " + arreglo1[i]);
+            // }
+
+            string[] nombres = new string[5];
+
+            for(i = 0; i < 5; i++){
+                Console.WriteLine(i);
+                nombres[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Indice      Valor");
+            for(i = 0; i < 5; i++){
+                Console.WriteLine("  " + i + "           " + nombres[i]);
+            }
         }
     }
 }
---
+++ NombreYEdadesConArreglos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NombreYEdadesConArreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[5];
+            int[] edades = new int[5];
+
+            //Guardar datos en un arreglo con un for
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("Cual es el nombre de la persona #" + (i+1) + "? ");
+                nombres[i] = Console.ReadLine();
+
+                Console.Write("Cual es la edad de la persona? ");
+                edades[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+            }
+            //Mostrar el contenido del arreglo con un for
+            Console.WriteLine("NOMBRES   EDADES");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(nombres[i] + "  " + edades[i]);
+            }
+        }
+    }
+}
